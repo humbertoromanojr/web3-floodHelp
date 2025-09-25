b8cbc66314b8902d4ddef3e0866f33119d533c03
Humberto Junior <astronomi@gmail.com>
2025-09-25 19:58:34 -0300

    Created Header component

---
+++ dapp/src/components/Header.js
@@ -0,0 +1,31 @@
+export default function Header() {
+  return (
+    <header className="p-3 text-bg-dark">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center">
+          <a
+            href="/"
+            className="justify-content-start"
+            style={{ textDecoration: "none" }}
+          >
+            <h1 className="fw-bold text-light">FloodHelp</h1>
+          </a>
+          <div className="col-9 text-end">
+            <button type="button" className="btn btn-outline-light me-2">
+              <img
+                src="/logo-metamask.png"
+                alt="logo metamask"
+                width="24"
+                className="me-3"
+              />
+              Log In
+            </button>
+            <a href="/create" className="btn btn-warning">
+              Help me, please
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
