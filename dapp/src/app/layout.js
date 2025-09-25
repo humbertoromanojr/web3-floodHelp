29d7be06e35c5162a3f660c1ddf1dabc5d11740a
Humberto Junior <astronomi@gmail.com>
2025-09-25 18:02:01 -0300

    Updates settings application

---
+++ dapp/src/app/layout.js
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "FloodHelp",
+  description: "Ajude os desabrigados pelas enchentes",
+  charSet: "utf-8",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></head>
+      <body>{children}</body>
+    </html>
+  );
+}
