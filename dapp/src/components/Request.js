e4d6f6dfbd90b8ccb66b50ebfd7b99d24d3e6665
Humberto Junior <astronomi@gmail.com>
2025-09-26 14:46:06 -0300

    import generateAvatarURL  from cfx-kit/wallet-avatar

--- dapp/src/components/Request.js
+++ dapp/src/components/Request.js
@@ -1,3 +1,16 @@
+import { generateAvatarURL } from "@cfx-kit/wallet-avatar";
 export default function Request() {
-  return <div>Request</div>;
+  return (
+    <>
+      <div className="list-group-item list-group-item-action d-flex gap-3 py-3">
+        <img
+          src={generateAvatarURL}
+          alt="Wallet"
+          width="32"
+          height="32"
+          className="rounded-circle flex-shrink-0"
+        />
+      </div>
+    </>
+  );
 }
