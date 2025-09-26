b51002049769e873b54fb5459f86163779cda5a2
Humberto Junior <astronomi@gmail.com>
2025-09-26 19:14:45 -0300

    Updated files

--- dapp/src/app/create/page.js
+++ dapp/src/app/create/page.js
@@ -1,14 +1,125 @@
+"use client";
+
+import { useState } from "react";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function Create() {
+  const [request, setRequest] = useState({
+    title: "",
+    description: "",
+    contact: "",
+    amount: 0,
+  });
+
+  function onInputChange(e) {
+    setRequest((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  function handleSave() {
+    alert("Starting the request saving process...please wait...");
+
+    openRequest(request)
+      .then((result) => {
+        alert(
+          "Request successfully created. It will be available on the home page in a few minutes!"
+        );
+        window.location.href = "/";
+        console.log(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert(err.message);
+      });
+  }
+
   return (
     <>
       <Header />
       <div className="container">
-        <div className="p-4 mx-5">Create Page</div>
-        <Footer />
+        <div className="ps-5">
+          <div className="row my-3">
+            <p className="lead text-light">
+              Please fill in all the fields below to tell us what you need.
+            </p>
+            <div className="col-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  id="title"
+                  className="form-control"
+                  maxLength={150}
+                  value={request.title}
+                  onChange={onInputChange}
+                />
+                <label htmlFor="title">Summary of what you need:</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <textarea
+                  type="text"
+                  id="description"
+                  className="form-control"
+                  style={{ height: 100 }}
+                  value={request.description}
+                  onChange={onInputChange}
+                />
+                <label htmlFor="description">
+                  Describe in detail what you need, and where you are (for
+                  in-person deliveries):
+                </label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  id="contact"
+                  className="form-control"
+                  maxLength={150}
+                  value={request.contact}
+                  onChange={onInputChange}
+                />
+                <label htmlFor="contact">Contact by (phone or email):</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  type="number"
+                  id="goal"
+                  className="form-control"
+                  value={request.goal}
+                  onChange={onInputChange}
+                />
+                <label htmlFor="goal">
+                  Metamask in BNB (leave blank if you do not wish to receive
+                  donations in crypto):
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="row my-3">
+            <div className="col-2">
+              <a href="/" className="btn btn-outline-warning p-3 col-12">
+                Back
+              </a>
+            </div>
+            <div className="col-4">
+              <button
+                type="button"
+                className="btn btn-warning p-3 col-12"
+                onClick={handleSave}
+              >
+                Send request
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -1,20 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import * as dotenv from "dotenv";
+//dotenv.config();
+console.log("dotenv  =>", process.env);
+
+import { getOpenRequests } from "@/services/Web3Service";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Request from "@/components/Request";
 
 export default function Home() {
+  const [lastId, setLastId] = useState(0);
+  const [requests, setRequests] = useState([]);
+
+  async function loadRequests() {
+    try {
+      const result = await getOpenRequests(lastId);
+
+      if (lastId === 0) {
+        setRequests(result);
+      } else {
+        requests.push(...result);
+        setRequests(requests);
+      }
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  }
+
+  useEffect(() => {
+    loadRequests(0);
+  }, []);
+
   return (
     <>
       <Header />
       <div className="container">
-        <div className="row ps-5">
-          <p className="text-light">
-            Ajude as vítimas de enchentes e demais desastres naturais.
-            <br />
-            <br />
+        <div className="row ps-5 my-3">
+          <p className="lead text-light">
             Help victims of floods and other natural disasters.
           </p>
         </div>
-        <div className="p-4 mx-5"></div>
+        <div className="p-4 mx-5">
+          <div className="list-group">
+            {requests && requests.length ? (
+              requests.map((rq) => <Request key={rq.id} data={rq} />)
+            ) : (
+              <div className="list-group-item p-5">
+                <p>
+                  Connect your Metamask wallet using the ‘Login’ button to help
+                  or ask for help.
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
         <Footer />
       </div>
     </>
